8adbaeabe13d46dfcf383328cc6dad6eae8c613c
Paul Teixeira <paul.teixeira@hotmail.com>
2013-11-15 00:29:21 -0700

    New CSS, themes and navigation menus
    I redid the css and navigation as per the conversation we had. Hope this
    is a better direction :D

--- ui/footer.php
+++ ui/footer.php
@@ -3,4 +3,15 @@
 Powered by Apex Solutions <br/>
 Copyright 2013 &copy
 </div>
+			<?php
+				//if user is logged in display additional menu options
+				if(isset($_SESSION['username']))
+				{
+				echo "<div id='loginname'>Logged in as: ".$_SESSION['username']." </div>";
+				echo "<ul id='adminmenu' class='menu'>";
+				echo "<li><a href=''><span>Add Packages</span></a></li>";
+				echo "<li><a href='logout.php'><span>Log Out</span></a></li>";
+				echo "</ul>";
+				}
+				?>
 </html>
--- ui/header.php
+++ ui/header.php
@@ -8,34 +8,11 @@ session_start();
 <head>
 	<body>
 		<div id='headbar'>
-			<div id='logo'><img src='./media/holder.jpg'></div>
-			<h1>Travel Experts</h1>
-			<?php
-			if(isset($_SESSION['username']))
-			{
-			echo "<div id='loginname'>Logged in as: ".$_SESSION['username']." </div>";
-			echo "<div id='logoutbutton'><a href='logout.php'>Logout</a></div>";
-			
-			}
-			//echo "<div id='loginname'>Logged in as: Joe</div>";
-			
-			?>
-		</div>
-		</div>
-		<div id='sidebar'>
-			<ul id='menu'>
-				<li><a href='index.php'>Home</a></li>
-				<li><a href='order.php'>Order</a></li>
-				<li><a href='contactpage.php'>Contact</a></li>
-				<li><a href='login.php'>LogIn</a></li>
-				<?php
-				//if user is logged in display additional menu options
-				if(isset($_SESSION['username']))
-				{
-				echo "<div style='float: left;font-size: 18;margin:5;'>Administratior Menu</div>";
-				echo "<li><a href='index.php'>Add Packages</a></li>";
-				echo "<li><a href='signup.php'>Reports</a></li>";
-				}
-				?>
+			<div id='logo'><img src='./media/websitelogo.png'></div>
+			<ul class="menu">
+            <li><a href='index.php'><span>Home</span></a></li>
+            <li><a href='order.php'><span>Order</span></a></li>
+            <li><a href='contactpage.php'><span>Contact</span></a></li>
+			<li><a href='login.php'><span>Log In</span></a></li>
 			</ul>
 		</div>
