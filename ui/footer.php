2338acc0b88566e557ad65bc00b84aa96a9dbe50
Paul Teixeira <paul.teixeira@hotmail.com>
2013-11-18 18:55:16 -0700

    Revert "corrected database, added links"
    This reverts commit 761237c0ea5d905ba443f4d91f846a1f2ad2fd8c.

--- ui/footer.php
+++ ui/footer.php
@@ -9,8 +9,7 @@ Copyright 2013 &copy
 				{
 				echo "<div id='loginname'>Logged in as: ".$_SESSION['username']." </div>";
 				echo "<ul id='adminmenu' class='menu'>";
-				echo "<li><a href='packageentry.php'>Add Packages</a></li>";
-				echo "<li><a href='report.php'>Report</a></li>";
+				echo "<li><a href=''>Add Packages</a></li>";
 				echo "<li><a href='logout.php'>Log Out</a></li>";
 				echo "</ul>";
 				}
