1d6125875217d73fd1486180199dbb4cb9ac11a7
modium <rtql8d@gmail.com>
2013-11-20 13:58:12 -0700

    text justified

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php include "ui/header.php"?>
 		<div id='content'>
-			<div>
+			<div id='text'>
 			<h2>Welcome to Travel Experts!</h2>
-			We are your new number one hub connecting you with </br>
-			destinations around the world! Whether you are a </br>
-			student, a couple or good old adventurers, we can </br>
-			assure you that we have the right package tailored </br>
-			specifically for you! </br>			
+			We are your new number one hub connecting you with 
+			destinations around the world! Whether you are a 
+			student, a couple or good old adventurers, we can 
+			assure you that we have the right package tailored 
+			specifically for you! 			
 			<h2>About Travel Experts</h2>
-			The Travel Experts is one of the earliest travel </br>
-			websites in the history of the web. We have updated </br>
-			our online presence to provide a more contemporary </br>
-			look for our customers, old and new. The new </br>
-			interface has been designed to be more intuitive </br>
-			and allow people to register and make bookings with </br>
-			as little delay as possible. So...what are you </br>
-			waiting for?</div>
-			<div id='box2'>
+			The Travel Experts is one of the earliest travel 
+			websites in the history of the web. We have updated 
+			our online presence to provide a more contemporary 
+			look for our customers, old and new. The new 
+			interface has been designed to be more intuitive 
+			and allow people to register and make bookings with 
+			as little delay as possible. So...what are you
+			waiting for?
+			</div>
+			<div id='box'>
 				<img src="media/home_img.jpg" height="600px" width="600px">
 			</div>	
 		</div>
